ffb9f0b9417338ee1087ec092cc014b6c256dad7
WindfuryARS <52396721+WindfuryARS@users.noreply.github.com>
2022-06-06 23:00:49 +0800

    Update 2nd_example.py

--- OOP(part2)/2nd_example/2nd_example.py
+++ OOP(part2)/2nd_example/2nd_example.py
@@ -21,3 +21,6 @@ class Calculation:
 timmy = Calculation(5, 8)
 print(timmy.square())
 print(timmy.add())
+
+#timmy is a certain instance to have specific attribute a=5, b=8, maybe you have other instance jim, oliver etc...
+#encapslute all operation/attribute in in class Calculation, just call method() to get what you want
