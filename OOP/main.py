08627088ba1a90af207954658e51bd421daae5fe
WindfuryARS <hancai@outlook.com>
2022-06-06 15:44:46 +0800

    OOP(part1)

---
+++ OOP/main.py
@@ -0,0 +1,16 @@
+import turtle
+
+qianlu = turtle.Turtle()
+
+# or
+
+from turtle import Turtle, Screen
+
+zhanma = Turtle()
+
+my_screen = Screen()
+print(my_screen.canvwidth)  # get screen width, canvwidth is !!!attribute!!! of Screen.  example.xx.canvwidth is
+# !!!attribute!!! of xx.
+
+zhanma.shape("turtle")  # this is !!!method!!! of Turtle.  example.xx() is !!!method!!! of xx.
+my_screen.exitonclick()  # this is !!!method!!! of Screen.  example.xx() is !!!method!!! of xx.
